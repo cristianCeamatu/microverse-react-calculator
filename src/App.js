3cb72b83eabdc02284bbe89e9affe8d966a48d6b
Cristian Viorel Ceamatu <cristian.ceamatu@gmail.com>
2020-10-27 17:59:12 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-danger my-3">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
